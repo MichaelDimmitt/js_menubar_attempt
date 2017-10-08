ceb08267617096e6d8d9f1a2594f893219036eb4
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-10-07 23:55:45 -0400

    pass screensaver as a parameter to downlod.

--- Aerial_Desktop/.hide/app/install/1_download_aerial.sh
+++ Aerial_Desktop/.hide/app/install/1_download_aerial.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
-
+screen=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # run aerial application
-  if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
+  if [ -d ~/Library/Screen\ Savers/$screen ] || [ -d /Library/Screen\ Savers/$screen ] ; then
     echo "File exists."
   else
-    open $DIR/../../bin/Aerial.saver/
+    open $DIR/../../bin/$screen/
   fi
