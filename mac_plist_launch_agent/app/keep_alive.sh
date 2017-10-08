2cc1a5caa4088ec364205ac1c02cac9a80af57b9
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-10-08 16:42:57 -0400

    update, menubar should be working 🤞

--- mac_plist_launch_agent/app/keep_alive.sh
+++ mac_plist_launch_agent/app/keep_alive.sh
@@ -1,3 +1,4 @@
 #bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-open $DIR/../../tutorial-menubar-app-darwin-x64/tutorial-menubar-app.app/
+# echo $(ls $DIR/../../../../../../)
+open $DIR/../../../../../../tutorial-menubar-app.app
